c8ad5547c23bde96fb4eea25e45549a8006eae34
jbeckmann1 <jo.bec@t-online.de>
2021-01-13 11:13:13 +0100

    Get the filepath of .test.js files

--- index.js
+++ index.js
@@ -1,2 +1,9 @@
 #!/usr/bin/env node
-console.log('Running test!!...');
+
+const Runner = require('./runner');
+const runner = new Runner();
+const run = async () => {
+	await runner.collectFiles(process.cwd());
+	console.log(runner.testFiles);
+};
+run();
